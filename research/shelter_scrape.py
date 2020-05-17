431535108867fc8e571f1fe3515ce8894ea96750
kec1510 <62352820+kec1510@users.noreply.github.com>
2020-05-16 22:52:57 -0400

    i scraped the directory for shelter listings in NYC

---
+++ research/shelter_scrape.py
@@ -0,0 +1,36 @@
+import pandas as pd 
+
+table = pd.read_html("https://www.shelterlistings.org/city/new_york-ny.html")[0]
+
+data = {
+    'Name': [],
+    'Location': [], 
+    'Phone': [],
+    'Description': []
+}
+for row in table[0]:
+    row = row.strip('View Full Listing Details')
+    row = row.split('  ')
+
+    shelter_name = row[0]
+    shelter_location = row[1]
+    if len(row) > 2:
+        shelter_phone = row[2]
+    else:
+        shelter_phone = None
+
+    if len(row) > 3:
+        shelter_desc = row[3]
+    else:
+        shelter_desc = None
+
+    data['Name'].append(shelter_name)
+    data['Location'].append(shelter_location)
+    data['Phone'].append(shelter_phone)
+    data['Description'].append(shelter_desc)
+
+df = pd.DataFrame(data)
+df.to_csv('nyc_shelters.csv')
+print('Done!')
+
+
