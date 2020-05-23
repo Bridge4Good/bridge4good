450b63aea8609c9e2387482debb03177c8bfc122
kec1510 <62352820+kec1510@users.noreply.github.com>
2020-05-23 16:48:19 -0400

    scraped a NY small business directory

---
+++ research/business_scrape.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests as req 
+import pandas as pd
+
+page = req.get('http://nybizlist.com/nyc-small-business-directory.htm')
+
+soup = BeautifulSoup(page.text, 'lxml')
+
+listings = soup.find_all(class_='featured')
+
+businesses = []
+for biz in listings:
+    biz = biz.text.replace('\r\n\t\t\t\t\t', '')
+    biz = biz.replace('(map)', '').replace('\xa0 |\xa0', '').replace('\n', '')
+    businesses.append(biz)
+
+df = pd.DataFrame(businesses)
+df.to_csv('nyc_businesses.csv')
